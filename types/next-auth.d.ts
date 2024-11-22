1f14e8a25f3ef33555b99589c73024e2f5b568f7
Thiago Silva <thiago.silva>
2024-11-22 17:58:05 -0300

    feat: 🎸 lint

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,21 +1,21 @@
 //types/next-auth.d.ts
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type { UserRole } from "@prisma/client";
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession } from 'next-auth';
+import type { UserRole } from '@prisma/client';
 
-export type ExtendedUser = DefaultSession["user"] & {
+export type ExtendedUser = DefaultSession['user'] & {
   id: string;
   role: UserRole;
 };
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
     user: ExtendedUser;
   }
 }
 
-declare module "@auth/core/jwt" {
+declare module '@auth/core/jwt' {
   interface JWT {
     role?: UserRole;
   }
